6a827a08494258422bdbbf99de61643c68849f81
enseor <enriqueseor@gmail.com>
2026-01-30 12:49:37 +0100

    Refactor: Simplify image modal, remove title and close button

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const modal = document.getElementById("imageModal");
     const modalImg = document.getElementById("imgFull");
-    const captionText = document.getElementById("caption");
     const cards = document.querySelectorAll(".art-card img");
 
     cards.forEach(img => {
         img.addEventListener("click", () => {
             modal.style.display = "flex";
             modalImg.src = img.src;
-            const title = img.parentElement.querySelector("h3").innerText;
-            captionText.innerHTML = title;
             document.body.style.overflow = "hidden"; 
         });
     });
